77a4db4dadf25fc62a6e5d885b33753e1f7236ba
guizinsx <guizin51@gmail.com>
2024-03-21 22:35:31 -0300

    cadastrar novo carro

--- src/tabelaCarros.jsx
+++ src/tabelaCarros.jsx
@@ -3,6 +3,13 @@ import React, { useState, useEffect } from 'react';
 const TabelaCarros = () => {
   const [carros, setCarros] = useState([]);
   const [carroSelecionado, setCarroSelecionado] = useState(null); // Estado para armazenar o carro selecionado para edição
+  
+  const [nome, setNome] = useState("");
+  const [ano, setAno] = useState("");
+  const [potencia, setPotencia] = useState("");
+  const [preco, setPreco] = useState("");
+  const [fabricante, setFabricante] = useState("");
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,10 +30,43 @@ const TabelaCarros = () => {
     setCarroSelecionado(carros[index]);
   };
 
-  const handleSaveEdit = () => {
-    // Implemente a lógica para salvar as alterações do carro editado no servidor
-    console.log('Salvar alterações:', carroSelecionado);
-    // Após salvar, você pode limpar o estado do carroSelecionado
+  const handleEditSave = (index) => {
+    // Define o carro selecionado para edição
+    setCarroSelecionado(carros[index]);
+  };
+  const handleSave = async (e) => {
+    e.preventDefault();
+
+    const formData = new URLSearchParams();
+    formData.append('nome', nome);
+    formData.append('ano', parseInt(ano));
+    formData.append('potencia', potencia);
+    formData.append('preco', parseInt(preco));
+    formData.append('fabricante', fabricante);
+
+    try {
+      const response = await fetch('https://ifsp.ddns.net/webservices/carro/carro',{
+        method:"POST", 
+        headers:{"Content-Type":"application/x-www-form-urlencoded"},
+        // body:{
+        //   id: 100,
+        //   nome: nome,
+        //   ano: parseInt(ano),
+        //   potencia: potencia,
+        //   preco: parseInt(preco),
+        //   fabricante: fabricante
+        // }
+        body:formData
+        //body:JSON.stringify(request)
+      });
+      const data = await response.json();
+      console.log(data)
+    } catch (error) {
+      console.error('Erro ao buscar os dados:', error);
+    }
+    console.log(nome, ano, potencia, preco, fabricante)
+  
+
     setCarroSelecionado(null);
   };
 
@@ -37,6 +77,29 @@ const TabelaCarros = () => {
 
   return (
     <div>
+      <form onSubmit={handleSave}>
+        <div className="formgroup"> 
+          <label htmlFor="nome">nome</label>
+          <input type="text" name="nome" value={nome} onChange={(e)=>setNome(e.target.value)}></input>
+        </div>
+        <div className="formgroup">
+          <label htmlFor="ano">ano</label>
+          <input type="text" name="ano" value={ano} onChange={(e)=>setAno(e.target.value)}></input>
+        </div>
+        <div className="formgroup">
+          <label htmlFor="potencia">potencia</label>
+          <input type="text" name="potencia" value={potencia} onChange={(e)=>setPotencia(e.target.value)}></input>
+        </div>
+        <div className="formgroup">
+          <label htmlFor="preco">preco</label>
+          <input type="text" name="preco" value={preco} onChange={(e)=>setPreco(e.target.value)}></input>
+        </div>
+        <div className="formgroup">
+          <label htmlFor="fabricante">fabricante</label>
+          <input type="text" name="fabricante" value={fabricante} onChange={(e)=>setFabricante(e.target.value)}></input>
+        </div>
+        <button>cadastrar</button>
+      </form>
       <h1>Tabela de Carros</h1>
       <table>
         <thead>
